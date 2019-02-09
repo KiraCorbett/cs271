ec25b8ed40ee34f8c6c2fc05090330551d728694
Kira Corbett <kirac2093@gmail.com>
2019-02-08 17:46:31 -0800

    Implemented Mult asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,39 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+ // Initialize R0
+@R0
+D=M
+
+// Initialize R1
+@R1
+D=M
+@val
+M=D
+
+// Initialize R2
+@R2
+M=0
+
+// Decrement a counter value (val), then increment original value to add to itself
+// and create a multiplier
+(LOOP)
+	@val
+	D=M
+	@END
+	D;JEQ // (if==0), goto END
+	@val
+	M=D-1 // decrement counter value
+
+	// Increment R2
+	@R0
+	D=M
+	@R2
+	M=D+M
+	@LOOP
+	0;JMP
+
+//Finish
+(END)
+@END
+0;JMP
