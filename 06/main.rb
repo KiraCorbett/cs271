a5e8fec6021c81ba9af318bba17b5524e46d764e
Kira Corbett <kirac2093@gmail.com>
2019-02-25 16:59:10 -0800

    Updated some functions

--- 06/main.rb
+++ 06/main.rb
@@ -1,6 +1,7 @@
 load 'parser.rb'
+load 'code.rb'
+load 'symbol_table.rb'
 
 parser = Parser.new(ARGV[0])
 
 parser.parse()
-
--- 06/parser.rb
+++ 06/parser.rb
@@ -1,7 +1,13 @@
 # Parses access input code; reads command, parses it, and provides access to fields
 # and symbols. Removes whitespace and comments. 
 
+#load 'symbol_table.rb'
+#load 'code.rb'
+
 class Parser
+
+  #symbolTable = symbol_table.new
+  #coder = code.new
 
 	# prepares to parse file
 	def initialize(file)
@@ -15,10 +21,27 @@ class Parser
 			next if line.strip[0] == '/'
 			next if line.strip.empty?
 
+			#if 'A_COMMAND' 
+			#	binary = number.to_s(2).rjust(16, '0')
+			#elsif 'A_COMMAND' and 'L_COMMAND'
+				# 	throw symbol through symbol table and convert resulting number to binary
+			#elsif 'C_COMMAND'
+				#coder.binary
+				#coder.comp
+				#coder.dest
+				#coder.jmp
+
 			puts line
 			puts commandType(line)
 		end
 	end
+
+	# If A command and just a number
+  # 	convert number to binary
+  # elsif A command and symbol
+  # 	throw symbol through symbol table and convert resulting number to binary
+	# elsif C command
+  # => do c command stuff
 
 	# returns type of command
 	def commandType(line)
@@ -49,5 +72,4 @@ class Parser
 			line.split(';').last
 		end
 	end	
-
 end
